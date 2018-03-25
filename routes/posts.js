9f046e8ba3364d8bc9672b7d46c336bde2f76738
Rostradamus <hyungro@hotmail.com>
2018-03-25 13:56:48 -0700

    minor fix: order post by descending order

--- routes/posts.js
+++ routes/posts.js
@@ -3,18 +3,19 @@ const Post = mongoose.model('Post');
 
 module.exports = app => {
 
-    app.get('/api/posts', (req, res) => {
-        Post.find({}, (err, posts) => {
-            if (err) res.send(err);
-            res.send(posts);
-        });
-    });
+  app.get('/api/posts', (req, res) => {
+    let order = { time: -1 };
+    Post.find({}).sort(order).exec((err, posts) => {
+        if (err) res.send(err);
+        res.send(posts);
+      });
+  });
 
-    app.post('/api/posts/new', (req, res) => {
-        const post = new Post(req.body);
-        post.save((err, target) => {
-            if (err) res.send(err);
-            res.send(target);
-        });
-    });
+  app.post('/api/posts/new', (req, res) => {
+      const post = new Post(req.body);
+      post.save((err, target) => {
+          if (err) res.send(err);
+          res.send(target);
+      });
+  });
 };
